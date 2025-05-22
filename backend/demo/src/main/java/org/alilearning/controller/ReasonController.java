dde3094c2f58a49ac6b28bd33aa13207dbbdfd73
Ali McNamara <allisonrm95@gmail.com>
2025-05-22 15:31:05 -0400

    still trouble shooting

--- backend/demo/src/main/java/org/alilearning/controller/ReasonController.java
+++ backend/demo/src/main/java/org/alilearning/controller/ReasonController.java
@@ -4,6 +4,7 @@ import org.alilearning.model.Reason;
 import org.alilearning.service.ReasonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,10 @@ public class ReasonController {
     public ReasonController(ReasonService reasonService){
         this.reasonService = reasonService;
     }
-
+    @GetMapping("/test")
+    public String test() {
+        return "API is working!";
+    }
     @PostMapping("/reasons")
     public Reason createReason(@RequestBody Reason reason){
         return reasonService.createReason(reason.getReason());
